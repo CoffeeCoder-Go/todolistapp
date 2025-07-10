3ca4721f23e4fa31a6b5f347f69ad814f00d9595
CoffeeCoder-Go <henriqueprogramador2@gmail.com>
2025-07-09 20:38:50 -0400

    feat(nt):Added a link to login form and the init of login form dev

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
+import axios from 'axios';
 
 const app = express();
 
@@ -10,8 +11,14 @@ app.set("views","./views");
 // Arquivos CSS e JS
 app.use(express.static('./public'))
 
-app.get("/",(req,res)=>{
-    res.render("home");
+app.get("/",async (req,res)=>{
+    let response = await axios.get('http://localhost:3000/');
+    console.log(response.data.msg);
+    res.render("home",response.data);
+});
+
+app.get("/signup",async (req,res)=>{
+	res.render("login");
 });
 
 app.listen(4000);
--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+)
 
 func main(){
 	router := gin.Default()
 
 	router.GET("/",func (c *gin.Context){
 		c.JSON(200,gin.H{
-			"msg":"Hello World",
+			"msg":"Hello World!",
 		})
 	})
+
+	router.Use(cors.Default())
 	router.Run(":3000")
 }
